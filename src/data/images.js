a60aa5f8fef584e455d85b5a8792d6c01937e4c3
Mufudzi Christopher <mnhamoinesu@hotmail.com>
2025-07-14 16:54:30 +0200

    SMall fix with the spiral

--- src/data/images.js
+++ src/data/images.js
@@ -6,8 +6,6 @@ const images = [
     { url: '/images/img5.webp' },
     { url: '/images/img6.webp' },
     { url: '/images/img7.webp' },
-    { url: '/images/img8.webp' },
-    { url: '/images/img9.webp' },
     { url: '/images/img10.webp' },
     { url: '/images/img11.webp' },
     { url: '/images/img12.webp' },
